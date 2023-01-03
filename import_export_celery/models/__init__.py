01d17c95355ed7905207409f38cb5f6adf95374a
Oliver Herrera <oliver.herrera@spacebase.com>
2023-01-03 14:02:37 +0100

    Adds AbstractExportJob to exported models

--- import_export_celery/models/__init__.py
+++ import_export_celery/models/__init__.py
@@ -1,10 +1,11 @@
 # Copyright (C) 2019 o.s. Auto*Mat
 
 """Import all models."""
-from import_export_celery.models.exportjob import ExportJob
+from import_export_celery.models.exportjob import AbstractExportJob, ExportJob
 from import_export_celery.models.importjob import ImportJob
 
 __all__ = (
+    AbstractExportJob,
     ExportJob,
     ImportJob,
 )
